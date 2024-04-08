aee80f07f524ce5e8fc186ff8b7f5e568a97558e
Charchuta Mikolaj Mieszko <charchutam@e-btlnet.local>
2024-04-08 13:17:16 +0200

    Create functions to exxtract k-mers from sequences

--- tests/testthat/test-kmers.R
+++ tests/testthat/test-kmers.R
@@ -2,13 +2,25 @@ test_that("Can extract all possible 8-mers from a sequence", {
 
   x <- "ATGCGCTAGTAGCATGC"
 
+  all_kmers <- get_all_kmers(x, kmer_size = 8)
+
   expected_kmers <- c("ATGCGCTA", "TGCGCTAG", "GCGCTAGT", "CGCTAGTA",
                       "GCTAGTAG", "CTAGTAGC", "TAGTAGCA", "AGTAGCAT",
                       "GTAGCATG","TAGCATGC")
 
   all_kmers <- get_all_kmers(x)
+  expect_equal(all_kmers, expected_kmers)
+  expect_length(all_kmers, length(expected_kmers))
+
+
+  all_kmers <- get_all_kmers(x, kmer_size = 9)
+
+  expected_kmers <- c("ATGCGCTAG", "TGCGCTAGT", "GCGCTAGTA", "CGCTAGTAG",
+                      "GCTAGTAGC", "CTAGTAGCA", "TAGTAGCAT", "AGTAGCATG",
+                      "GTAGCATGC")
 
   expect_equal(all_kmers, expected_kmers)
+  expect_length(all_kmers, length(expected_kmers))
 
 })
 
@@ -26,6 +38,13 @@ test_that("Can extract specific kmer from a starting position and size", {
   expect_equal(kmer, "TAGCATGC")
 
   expect_error(get_kmer(x, 11, kmer_size = 8))
+
+  kmer <- get_kmer(x, 5)
+  expect_equal(kmer, "GCTAGTAG")
+
+
+
+
 })
 
 
